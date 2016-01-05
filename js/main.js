c42b5b2a4f4fe6a48cfe9cc698a5999ac19c4586
chchurch7 <Chris@Chriss-MBP.fios-router.home>
2016-01-05 16:31:46 -0500

    Remaining pages and mobile menu

--- js/main.js
+++ js/main.js
@@ -4,6 +4,45 @@ $(document).ready(function(){
     '.single-item': 2
   });
 });
+var menu = $('#mobileMenu')
+
+
+$('#mobileButton').click(function(event){
+  event.preventDefault();
+  if (menu.is(":visible")) {
+    menu.slideUp(400);
+    $(this).removeClass("open");
+    $(this).css({
+    'background-image': 'url("img/menu.svg")',
+    'height': '20px',
+    'width': '20px'
+  })
+
+  }
+  else {
+    menu.slideDown(400);
+    $(this).addClass("open");
+    $(this).css({
+    'background-image': 'url("img/close.svg")',
+    'height': '20px',
+    'width': '20px'
+  })
+  }
+});
+
+
+// $( "#mobileButton" ).click(function() {
+//   $( "#mobileMenu" ).slideDown( "slow", function() {
+//     // Animation complete.
+//   });
+//    $('#mobileButton').css({
+//         'background-image': 'url("img/close.svg")',
+//         'height': '20px',
+//         'width': '20px'
+//   });
+//    else
+// });
+
 
 $(function(){
 	$('.mobile-nav-trigger').click(function(){
@@ -15,3 +54,4 @@ $(function(){
     return false;
 	});
 });
+
